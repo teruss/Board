375aaaa58b09c0b750f1a2f18df413f52003e365
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2016-02-21 22:20:32 +0900

    Add Drop class

---
+++ System/Drop.cs
@@ -0,0 +1,27 @@
+﻿namespace Board
+{
+    public class Drop : Command
+    {
+        IGameController controller;
+
+        public Drop(IPiece piece, IGameController controller) : base(piece)
+        {
+            this.controller = controller;
+        }
+
+        public override void Execute()
+        {
+            piece.captured = false;
+            piece.promoted = false;
+            controller.GetKomadai(piece.opposed).Drop(piece);
+            piece.UpdateSprite();
+            piece.activated = true;
+        }
+
+        public override void Undo()
+        {
+            controller.GetKomadai(piece.opposed).Accept(piece);
+            base.Undo();
+        }
+    }
+}
