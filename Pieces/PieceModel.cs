b7db822443c58a65dbdcec84e7c25587d574079d
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2016-02-25 21:47:21 +0900

    Add PieceModel

---
+++ Pieces/PieceModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Board
+{
+    public class PieceModel
+    {
+        public int column { get; set; }
+        public int row { get; set; }
+        public PieceType type { get; set; }
+        public bool opposed { get; set; }
+
+        public PieceModel(int column, int row, PieceType type, bool opposed)
+        {
+            this.column = column;
+            this.row = row;
+            this.type = type;
+            this.opposed = opposed;
+        }
+    }
+}
