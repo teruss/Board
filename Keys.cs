2c4048fb9a64da55b939e203c1d225018d058ae0
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2016-06-26 01:16:40 +0900

    Add key

--- Keys.cs
+++ Keys.cs
@@ -4,5 +4,6 @@
     {
         public static readonly string Alternate = "Alternate";
         public static readonly string FinishOnKingKilled = "FinishOnKingKilled";
+        public static readonly string OrganizeKomadai = "OrganizeKomadai";
     }
 }
