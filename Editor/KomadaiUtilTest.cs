87a5921282b2c183dde2912cd8aaedf62e665d42
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2016-07-31 01:08:53 +0900

    Adjust piece positions when pieces is 4 on table

--- Editor/KomadaiUtilTest.cs
+++ Editor/KomadaiUtilTest.cs
@@ -20,4 +20,11 @@ public class KomadaiUtilTest
     {
         Assert.That(KomadaiUtil.Calc(0, 3), Is.EqualTo(KomadaiUtil.Calc(3, 9)));
     }
+
+    [Test]
+    public void FourPiecesTest()
+    {
+        Assert.That(KomadaiUtil.Calc(0, 4), Is.EqualTo(KomadaiUtil.Calc(5, 16)));
+        Assert.That(KomadaiUtil.Calc(2, 4), Is.EqualTo(KomadaiUtil.Calc(9, 16)));
+    }
 }
--- Komadai/KomadaiUtil.cs
+++ Komadai/KomadaiUtil.cs
@@ -13,24 +13,26 @@
         {
             if (count == 1)
                 return 1.8f;
-            if (count == 2)
-                return 2.3f - i;
+            if (count == 2 || count == 4)
+                return 2.3f - i % 2;
             if (count <= 9)
                 return 2.8f - i % 3;
             if (count <= 16)
-                return 3.2f - i % 4;
-            return 0.2f + i / 4;
+                return 3.3f - i % 4;
+            return 0.3f + i / 4;
         }
 
         static float CalcY(int i, int count)
         {
             if (count <= 3)
                 return 12.2f;
+            if (count == 4)
+                return 11.7f + i / 2;
             if (count <= 9)
                 return 11.2f + i / 3;
             if (count <= 16)
-                return 10.5f + i / 4;
-            return 10.5f + i % 4;
+                return 10.7f + i / 4;
+            return 10.7f + i % 4;
         }
     }
 }
