7617fe3b0737f33207f2068df96c20f49016abc1
teruss <teruyoshi.sasaki@gmail.com>
2016-10-13 00:26:08 +0900

    Refactoring

--- PieceModel/LanceModel.cs
+++ PieceModel/LanceModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Board
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace Board
 {
     public class LanceModel : PieceModel
     {
@@ -9,60 +12,40 @@
 
         public override bool CanCheckAfterMove(World world, Location l, PieceModel piece)
         {
+            var enemyKing = world.PieceManager.GetEnemyKing(this);
+            if (enemyKing.Location.Column != Location.Column)
+            {
+                return false;
+            }
+            if (!IsOnTheLine(enemyKing))
+            {
+                return false;
+            }
+            if (Location.Column == l.Column)
+            {
+                return false;
+            }
+
+            var piecesBetween = world.PieceManager.GetPiecesBetween(this, enemyKing);
+            if (piecesBetween.Count != 1)
+            {
+                return false;
+            }
+            if (piecesBetween[0] != piece)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsOnTheLine(KingModel enemyKing)
+        {
             if (Player == Player.White)
-            {
-                var enemyKing = world.PieceManager.GetEnemyKing(this);
-                if (enemyKing.Location.Column != Location.Column)
-                {
-                    return false;
-                }
-                if (enemyKing.Location.Row > Location.Row - 2)
-                {
-                    return false;
-                }
-                if (Location.Column == l.Column)
-                {
-                    return false;
-                }
+                return enemyKing.Location.Row <= Location.Row - 2;
+            if (Player == Player.Black)
+                return enemyKing.Location.Row >= Location.Row + 2;
 
-                var piecesBetween = world.PieceManager.GetPiecesBetween(this, enemyKing);
-                if (piecesBetween.Count != 1)
-                {
-                    return false;
-                }
-                if (piecesBetween[0] != piece)
-                {
-                    return false;
-                }
-                return true;
-            }
-            else if (Player == Player.Black)
-            {
-                var enemyKing = world.PieceManager.GetEnemyKing(this);
-                if (enemyKing.Location.Column != Location.Column)
-                {
-                    return false;
-                }
-                if (enemyKing.Location.Row < Location.Row + 2)
-                {
-                    return false;
-                }
-                if (Location.Column == l.Column)
-                {
-                    return false;
-                }
-
-                var piecesBetween = world.PieceManager.GetPiecesBetween(this, enemyKing);
-                if (piecesBetween.Count != 1)
-                {
-                    return false;
-                }
-                if (piecesBetween[0] != piece)
-                {
-                    return false;
-                }
-                return true;
-            }
+            Assert.AreNotEqual(Player.Gray, Player);
             return false;
         }
     }
