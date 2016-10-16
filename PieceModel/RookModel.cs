26a255039eafa2475dec5355bdb66ad5290a5d9b
teruss <teruyoshi.sasaki@gmail.com>
2016-10-16 16:57:39 +0900

    Refactoring

--- PieceModel/RookModel.cs
+++ PieceModel/RookModel.cs
@@ -10,44 +10,52 @@
         public override Direction GetDirection(PieceManager manager, KingModel king, PieceModel piece)
         {
             var e = king.Location;
-
-            foreach (var p in manager.Pieces())
-            {
-                if (p == king || p == piece)
-                    continue;
-                var l = p.Location;
-                if (e.Column == l.Column && Location.Column == l.Column)
-                {
-                    if ((e.Row - l.Row) * (Location.Row - l.Row) < 0)
-                    {
-                        return Direction.AnyWhere;
-                    }
-                }
-                if (e.Row == l.Row && Location.Row == l.Row)
-                {
-                    if ((Location.Column - l.Column) * (e.Column - l.Column) < 0)
-                    {
-                        return Direction.AnyWhere;
-                    }
-                }
-            }
-
             var d = piece.Location;
 
             if (e.Column == d.Column && Location.Column == d.Column)
             {
-                if ((Location.Row - d.Row) * (e.Row - d.Row) < 0)
+                if ((Location.Row - d.Row) * (e.Row - d.Row) >= 0)
                 {
-                    return Direction.Vertical;
+                    return Direction.AnyWhere;
                 }
+                foreach (var p in manager.Pieces())
+                {
+                    if (p == king || p == piece)
+                        continue;
+                    var l = p.Location;
+                    if (e.Column == l.Column && Location.Column == l.Column)
+                    {
+                        if ((e.Row - l.Row) * (Location.Row - l.Row) < 0)
+                        {
+                            return Direction.AnyWhere;
+                        }
+                    }
+                }
+                return Direction.Vertical;
             }
+
             if (e.Row == d.Row && Location.Row == d.Row)
             {
-                if ((Location.Column - d.Column) * (e.Column - d.Column) < 0)
+                if ((Location.Column - d.Column) * (e.Column - d.Column) >= 0)
                 {
-                    return Direction.Horizontal;
+                    return Direction.AnyWhere;
                 }
+                foreach (var p in manager.Pieces())
+                {
+                    if (p == king || p == piece)
+                        continue;
+                    var l = p.Location;
+                    if (e.Row == l.Row && Location.Row == l.Row)
+                    {
+                        if ((Location.Column - l.Column) * (e.Column - l.Column) < 0)
+                        {
+                            return Direction.AnyWhere;
+                        }
+                    }
+                }
+                return Direction.Horizontal;
             }
+
             return Direction.AnyWhere;
         }
     }
