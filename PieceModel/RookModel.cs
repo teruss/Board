8eb3df6cd2c9159796af20e232e56f76799e5d89
teruss <teruyoshi.sasaki@gmail.com>
2016-10-29 18:38:04 +0900

    Refactoring

--- PieceModel/RookModel.cs
+++ PieceModel/RookModel.cs
@@ -12,9 +12,14 @@ namespace Board
         internal override bool IsBetween(Direction dir, KingModel king, PieceModel p)
         {
             var l = p.Location;
-            if (dir == Direction.Vertical)
-                if (king.Location.Column == l.Column && Location.Column == l.Column)
-                    return (king.Location.Row - l.Row) * (Location.Row - l.Row) < 0;
+
+            if (king.Location.Column == l.Column && Location.Column == l.Column)
+            {
+                if (dir == Direction.Up)
+                    return king.Location.Row < l.Row && l.Row < Location.Row;
+                if (dir == Direction.Down)
+                    return king.Location.Row > l.Row && l.Row > Location.Row;
+            }
             if (dir == Direction.Horizontal)
                 if (king.Location.Row == l.Row && Location.Row == l.Row)
                     return (Location.Column - l.Column) * (king.Location.Column - l.Column) < 0;
@@ -25,7 +30,12 @@ namespace Board
         internal override Direction CalcDirection(KingModel king)
         {
             if (king.Location.Column == Location.Column)
-                return Direction.Vertical;
+            {
+                if (king.Location.Row < Location.Row)
+                    return Direction.Up;
+                else
+                    return Direction.Down;
+            }
             if (king.Location.Row == Location.Row)
                 return Direction.Horizontal;
             return Direction.AnyWhere;
