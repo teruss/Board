58b0b7c41c67a87f78133a2f603c2ab864088474
Teruyoshi Sasaki <teruyoshi.sasaki@outlook.com>
2018-01-06 19:42:06 +0900

    Refactoring

--- Keys.cs
+++ Keys.cs
@@ -4,7 +4,9 @@
     {
         public static readonly string Alternate = "Alternate";
         public static readonly string FinishOnKingKilled = "FinishOnKingKilled";
-        public static readonly string DoAlways = "DoAlways";
         public static readonly string MovingTwice = "MovingTwice";
+        public const int ModeCheck = 0;
+        public const int ModeAllow = 1;
+        public const int ModeForbid = 2;
     }
 }
